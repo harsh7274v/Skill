d13b566399ce857bd56b52828d924e7fb8caacfe
HARSH VARDHAN PRASAD <114021101+harsh7274v@users.noreply.github.com>
2024-04-26 04:43:14 +0530

    first commit

---
+++ public/assets/js/cources.js
@@ -0,0 +1,24 @@
+// Sticky Navbar
+window.onscroll = function() {stickyNavbar()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyNavbar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
